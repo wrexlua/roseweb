cb3461f806acf890f1a8998da98f2c6f6997ed5f
wrexlua <ahmetkaanakhan1980@gmail.com>
2026-05-24 18:41:57 +0300

    fix: Vercel serves static natively, only /api/* to serverless

--- api/index.js
+++ api/index.js
@@ -1,9 +1,2 @@
-const path = require('path');
-const express = require('express');
-const serverApp = require('../server');
-
-const ROOT = path.resolve(__dirname, '..');
-serverApp.use(express.static(ROOT));
-serverApp.use('/admin', express.static(path.join(ROOT, 'admin')));
-
-module.exports = serverApp;
+const app = require('../server');
+module.exports = app;
