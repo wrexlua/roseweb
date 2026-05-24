0331369426606c960a926efcb21c4b5738cb2f2a
wrexlua <ahmetkaanakhan1980@gmail.com>
2026-05-24 18:40:52 +0300

    fix: add static file path fallback in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,9 @@
-const app = require('../server');
-module.exports = app;
+const path = require('path');
+const express = require('express');
+const serverApp = require('../server');
+
+const ROOT = path.resolve(__dirname, '..');
+serverApp.use(express.static(ROOT));
+serverApp.use('/admin', express.static(path.join(ROOT, 'admin')));
+
+module.exports = serverApp;
